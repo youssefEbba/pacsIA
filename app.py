f60983272d7c63abd689e6b1c665e42dbbea01bb
youssefyata <yousevyata@gmail.com>
2025-05-21 14:24:33 +0000

    'gdf'

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ def get_first_instance(study_uid, series_uid):
 def download_dicom_file(study_uid, series_uid, instance_uid, save_path):
     # Note the added /file at the end for actual DICOM file download
     file_url = f"{BASE_URL}/studies/{study_uid}/series/{series_uid}/instances/{instance_uid}/file"
+    # Use Accept header application/dicom to avoid 406
+    print(f"Downloading DICOM file from {file_url}")
     headers = {"Accept": "application/dicom; transfer-syntax=*"}
     response = requests.get(file_url, headers=headers)
     if response.status_code == 200:
